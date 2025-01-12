244f553c08370e0ab0475ba2a45fcba86ee8480c
telles <gabrieltellesm@gmail.com>
2025-01-12 20:09:23 -0300

    Done jlox scanner

---
+++ src/main/java/com/gabrieltelles/lox/Token.java
@@ -0,0 +1,8 @@
+package com.gabrieltelles.lox;
+
+record Token(TokenType type, String lexeme, Object literal, int line) {
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
