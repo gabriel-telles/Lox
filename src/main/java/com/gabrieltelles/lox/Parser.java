223765db1fbf8c6a3bc103a5f913741af4217dec
telles <gabrieltellesm@gmail.com>
2025-01-16 19:46:05 -0300

    Chapter 8: block statements

--- src/main/java/com/gabrieltelles/lox/Parser.java
+++ src/main/java/com/gabrieltelles/lox/Parser.java
@@ -46,6 +46,10 @@ class Parser {
             return printStatement();
         }
 
+        if (match(TokenType.LEFT_BRACE)) {
+            return new Stmt.Block(block());
+        }
+
         return expressionStatement();
     }
 
@@ -59,6 +63,17 @@ class Parser {
         Expr expr = expression();
         consume(TokenType.SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(TokenType.RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(TokenType.RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
     }
 
     private Expr expression() {
